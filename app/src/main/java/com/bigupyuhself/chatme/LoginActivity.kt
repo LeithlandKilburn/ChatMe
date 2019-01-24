127bbdfa18e0b421b2b2614e828d943525ff5fca
Leithland Kilburn <kilburnleithland@gmail.com>
2019-01-24 18:03:18 -0500

    I finished the login and sign-up scrren UI.

---
+++ app/src/main/java/com/bigupyuhself/chatme/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.bigupyuhself.chatme
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+
+class LoginActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_login)
+    }
+}
