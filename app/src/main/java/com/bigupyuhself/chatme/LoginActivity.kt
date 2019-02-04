8a359833cc0b5e213a1ac113416322bd1a768297
Leithland Kilburn <kilburnleithland@gmail.com>
2019-02-03 20:45:13 -0500

    Set up Firebase

--- app/src/main/java/com/bigupyuhself/chatme/LoginActivity.kt
+++ app/src/main/java/com/bigupyuhself/chatme/LoginActivity.kt
@@ -2,12 +2,29 @@ package com.bigupyuhself.chatme
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.content.*
 import android.os.PersistableBundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_login)
+
+        loginBUTLogin.setOnClickListener {
+            val email = emailETLogin.text.toString()
+            val password = passwordETLogin.text.toString()
+            Log.d("LoginActivity",  "Attempt login with email/pw: $email/***")
+
+            // Firebase authentication to create a user with email and password.
+
+        }
+
+        bakcTVLogin.setOnClickListener {
+            finish()
+        }
+
     }
 }
